951ea6f68c7739616ba25f1417a470a8072a9c97
Crowdbotics <team@crowdbotics.com>
2024-10-15 23:41:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-cell-49679",
+  url: "https://hidden-cell-49679.botics.co",
+  api: "https://hidden-cell-49679.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
